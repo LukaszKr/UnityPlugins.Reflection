28b1284808e38a2953f5aafd053341db2a968b15
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-08-25 20:54:39 +0200

    expose internal unity method to open code on specific line

---
+++ Unity/Assembly/AssemblyHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace ProceduralLevel.UnityPlugins.Reflection.Unity
+{
+	public static class AssemblyHelper
+	{
+		public static Type FindFirstMatching(string typeName)
+		{
+			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			int assemblyCount = assemblies.Length;
+			for(int x = 0; x < assemblyCount; ++x)
+			{
+				Assembly assembly = assemblies[x];
+				Type[] types = assembly.GetTypes();
+				int typeCount = types.Length;
+				for(int y = 0; y < typeCount; ++y)
+				{
+					Type type = types[y];
+					if(type.Name == typeName)
+					{
+						return type;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+}
