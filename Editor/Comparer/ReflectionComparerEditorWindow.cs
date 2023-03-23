2b8bd52faccaac5b11c634d9804eb11a43a17db9
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2023-03-23 17:49:54 +0100

    remove sample data from reflection comparer

--- Editor/Comparer/ReflectionComparerEditorWindow.cs
+++ Editor/Comparer/ReflectionComparerEditorWindow.cs
@@ -43,10 +43,7 @@ namespace ProceduralLevel.UnityPlugins.Reflection.Editor
 
 		protected override void Initialize()
 		{
-			ReflectionComparer comparer = new ReflectionComparer();
-			List<object> listA = new List<object>() { 1, 2, 3, 2, 5, 2, 2, 3, 4, 6, 8, 9, new List<object>() { 1, 2 } };
-			List<object> listB = new List<object>() { 2, 1, 3, 4, 1, 4, 4, 2, 5, 4, 2, 2, new List<object>() { 2, 3 }, 2 };
-			m_Diff = comparer.Compare(listA, listB);
+
 		}
 
 		protected override void Draw()
