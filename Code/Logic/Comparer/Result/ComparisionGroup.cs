78056b2f4e4ae17c425b5b8a06ef75949217e120
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2024-12-01 10:27:57 +0100

    implement to string for comparision group

--- Code/Logic/Comparer/Result/ComparisionGroup.cs
+++ Code/Logic/Comparer/Result/ComparisionGroup.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace UnityPlugins.Reflection.Logic
 {
 	public class ComparisionGroup
 	{
+		private const string PADDING_STR = "    ";
+
 		public readonly ComparisionGroup Parent;
 		public readonly string Key;
 		public readonly object Left;
@@ -20,6 +22,35 @@ namespace UnityPlugins.Reflection.Logic
 			Key = key;
 			Left = left;
 			Right = right;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			ToString(sb);
+			return sb.ToString();
+		}
+
+		public void ToString(StringBuilder sb, string depthPadding = "")
+		{
+			sb.Append(depthPadding);
+			sb.AppendLine($"{Key}");
+			depthPadding += PADDING_STR;
+
+			int entriesCount = Entries.Count;
+			for(int x = 0; x < entriesCount; ++x)
+			{
+				AComparisionResultEntry entry = Entries[x];
+				sb.Append(depthPadding);
+				sb.AppendLine(entry.ToString());
+			}
+
+			int subResultsCount = SubResults.Count;
+			for(int x = 0; x < subResultsCount; ++x)
+			{
+				ComparisionGroup subGroup = SubResults[x];
+				subGroup.ToString(sb, depthPadding);
+			}
 		}
 	}
 }
