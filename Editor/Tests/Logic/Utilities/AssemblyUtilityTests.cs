bd67577769edb95f1ddfd26367882a720c3b10dd
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2025-12-08 10:10:38 +0100

    assembly utility class

---
+++ Editor/Tests/Logic/Utilities/AssemblyUtilityTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace UnityPlugins.Reflection.Logic.Utilities
+{
+	[Category(ReflectionTestsConsts.CATEGORY_ASSEMBLY)]
+	internal class AssemblyUtilityTests
+	{
+		[Test]
+		public void FindAssembly_Returns()
+		{
+			Assert.IsNotNull(AssemblyUtility.FindAssembly("UnityPlugins.Reflection.Tests"));
+		}
+
+		[Test]
+		public void FindAssembly_ReturnNull_MissingAssembly()
+		{
+			Assert.IsNull(AssemblyUtility.FindAssembly("Assembly.That.Doesnt.Exist"));
+		}
+	}
+}
---
+++ Logic/Utilities/AssemblyUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace UnityPlugins.Reflection.Logic
+{
+	public static class AssemblyUtility
+	{
+		public static Assembly FindAssembly(string name)
+		{
+			foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				if(assembly.GetName().Name == name)
+				{
+					return assembly;
+				}
+			}
+
+			return null;
+		}
+	}
+}
